3ca58211d99ddd9f015c67517fd43873c82792d0
priyakumari-bu <pkumari@bu.edu>
2020-04-25 09:57:16 -0400

    updated account classes

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,4 +1,18 @@
 
-public class CheckingAccount {
+public class CheckingAccount extends Account{
+    // the fee charged to withdraw money from the account
+    private double withdrawalFee;
+
+    public CheckingAccount() {
+        setAccountType("checking"); 
+    }
+
+    public double getWithdrawalFee() {
+        return withdrawalFee;
+    }
+
+    public void setWithdrawalFee(double withdrawalFee) {
+        this.withdrawalFee = withdrawalFee;
+    }
 
 }
