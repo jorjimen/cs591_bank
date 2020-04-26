4a9e1ecdb08738befc94d0077fe645a114405450
sept9rain <920962588@qq.com>
2020-04-26 21:16:43 +0800

    Modified the BankUser, Customer and Manager

--- Manager.java
+++ Manager.java
@@ -1,4 +1,12 @@
 
-public class Manager {
+public class Manager extends BankUser {
+
+	// a method that prints all the info and details of the manager.
+    public void printUserInfo(){
+        System.out.println("Manager ID: " + id); 
+        System.out.println("Manager name: " + username); 
+        System.out.println("Manager password: " + password);
+        System.out.println(); 
+    }
 
 }
