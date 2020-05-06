ab576cb39ea3ee182be0ff0493784ffcc67fc9c7
sept9rain <920962588@qq.com>
2020-05-06 15:48:27 -0400

    add comment

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -2,7 +2,8 @@
 public class CheckingAccount extends Account{
     // the fee charged to withdraw money from the account
     private double withdrawalFee;
-
+    
+    // to set up the checking account
     public CheckingAccount(String currencyType) {
         super(currencyType); 
         setAccountType("checking"); 
